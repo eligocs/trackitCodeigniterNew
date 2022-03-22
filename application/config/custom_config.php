db72c5e7ac9065277cd99d7a991743fcf75bbfeb
eligocs <38901899+eligocs@users.noreply.github.com>
2022-03-22 16:23:06 +0530

    config files

---
+++ application/config/custom_config.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+//Default target for agent
+$config['monthly_target'] = 10;
+
+//google review link
+$config['google_review_link'] = "http://trackitinerary.com/";
+
+//EMAILS
+$config['accounts_team_email'] 		= "accounts1test@trackitinerary.in";
+$config['gm_email']					= "gmtest@trackitinerary.in";
+$config['cab_team_email']			= "cabtest@trackitinerary.in";
+$config['train_booking_email']		= "traintest@trackitinerary.in";
+$config['flight_booking_email']		= "flighttest@trackitinerary.in";
+
+//COMPANY INFO
+$config['pancard'] 			= "Company Pan Card";
+$config['gsttin'] 			= "Company Pan Gst";
+$config['hsn_sac_number'] 	= "Company Pan Card";
+
+//CGST and SGST in %
+$config['CGST'] 			= 2.50;
+$config['SGST'] 			= 2.50;
